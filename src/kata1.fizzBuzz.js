22bcc467443564e6bebd5c23e5889cbbc59dd1f9
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-05-27 23:30:52 +0100

    fizz buzz push

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,13 @@
-const fizzBuzz = number => {};
+const fizzBuzz = (number) => {
+  if (number % 5 === 0 && number % 3 === 0) {
+    return "FizzBuzz";
+  }
+
+  if (number % 5 === 0) {
+    return "Buzz";
+  }
+
+  return "Fizz";
+};
 
 module.exports = fizzBuzz;
