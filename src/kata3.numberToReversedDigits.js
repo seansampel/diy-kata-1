cdb0eb82a7a2a419a14e42244837768663344cb7
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-06-06 14:46:24 +0100

    push reverse numbers

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = (number) => {
+  return number
+    .toString()
+    .split("")
+    .map((x) => parseInt(x))
+    .reverse();
+};
 
 module.exports = numberToReversedDigits;
