03d2fb31db804ec3c8046a7c054ecdb1e3182920
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-06-07 15:48:34 +0100

    reach Destination task

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -3,7 +3,7 @@ const { reachDestination } = require("../src");
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
     expect(reachDestination(44, 10)).toBe(
-      "I should be there in about ${time} hour."
+      "I should be there in ${time} hours."
     );
   });
 });
