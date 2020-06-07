12261c3b3169923f44e190fc27062ebb16fd2d21
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-06-07 23:11:32 +0100

    push 3rd test to ensure it works

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -3,5 +3,7 @@ const { humanCatDogYears } = require("../src");
 describe("humanCatDogYears", () => {
   it("returns array of human, cat and dog years when passed human years", () => {
     expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(11)).toEqual([11, 60, 69]);
+    expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
   });
 });
