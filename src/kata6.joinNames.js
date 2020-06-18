e96a3ede033e79aea6a9e6b611e59078090d6f50
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-06-18 17:29:53 +0100

    push join names

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,7 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const array1 = namesObj.map((a) => a.name);
+  const array2 = array1.pop();
+  return array1.length < 1 ? `${array2}` : `${array1.join(", ")} & ${array2}`;
+};
 
 module.exports = joinNames;
