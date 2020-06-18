a11a0302e04c44e2e5ed7f78cc2cb9911ce2aa37
Sean <53915643+sampelmedia@users.noreply.github.com>
2020-06-18 18:11:42 +0100

    push final task

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,25 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  const employees = [
+    {
+      name: "Satti",
+
+      role: "Developer",
+    },
+    {
+      name: "Jenny",
+
+      role: "Sales Associate",
+    },
+    {
+      name: "Javid",
+
+      role: "Human Recommended Reading Assistant",
+    },
+  ];
+
+  it("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Satti", employees)).toBe("Developer");
+  });
 });
